9665aa76792693e8d56c97904e55772f7d184ad8
Sean Huber <shuber@huberry.com>
2011-02-17 11:44:23 -0800

    add empty tests

--- test/defined_test.rb
+++ test/defined_test.rb
@@ -1,4 +1,42 @@
 require 'test_helper'
 
 class DefinedTest < Test::Unit::TestCase
+  def test_should_call_defined_when_module_is_defined
+  end
+
+  def test_should_call_defined_when_module_is_defined_with_new
+  end
+
+  def test_should_call_defined_when_module_is_defined_with_class_eval
+  end
+
+  def test_should_call_defined_when_module_is_defined_with_instance_eval
+  end
+
+  def test_should_call_defined_when_module_is_defined_with_module_eval
+  end
+
+  def test_should_match_start?
+  end
+
+  def test_should_not_match_start?
+  end
+
+  def test_should_match_end?
+  end
+
+  def test_should_not_match_end?
+  end
+
+  def test_should_not_match_end_without_start
+  end
+
+  def test_trace_function_should_add_definition
+  end
+
+  def test_trace_function_should_remove_definition
+  end
+
+  def test_method_should_be_class_or_module_when_defined_with_keywords
+  end
 end
