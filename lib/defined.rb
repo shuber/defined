dde4eb98c8876f0b5bf6aca5d615d27323ff960c
Sean Huber <shuber@huberry.com>
2011-02-23 14:35:12 -0800

    fix Module#new bug

--- lib/defined.rb
+++ lib/defined.rb
@@ -1,41 +1,43 @@
-# Adds a <tt>Module.defined</tt> callback that is called whenever a class or module is defined or redefined
+# Calls <tt>Module.defined</tt> whenever a class or module is defined or redefined
 module Defined
   autoload :Version, 'defined/version'
 
-  # The default <tt>defined</tt> callback implementation which does nothing
-  def defined(file, line, method)
-  end
-
   instance_eval do
-    def definitions
-      @definitions ||= []
-    end
-
-    def definition?(event, method, klass, keyword_event, method_event)
-      event == keyword_event || (event == method_event && ((method == :new && klass.is_a?(Module)) || method.to_s =~ /^(class|instance|module)_eval$/))
-    end
-
-    def definition_start?(event, method, klass)
-      definition?(event, method, klass, 'class', 'c-call')
-    end
-
-    def definition_end?(event, method, klass)
-      !definitions.empty? && definition?(event, method, klass, 'end', 'c-return')
-    end
-
     def included(mod)
       set_trace_func method(:trace_function).to_proc
     end
 
-    def trace_function(event, file, line, method, binding, klass)
-      if definition_start?(event, method, klass)
-        definitions << binding.eval('self')
-      elsif definition_end?(event, method, klass)
-        object = definitions.pop
-        method ||= object.class.to_s.downcase.to_sym
-        object.defined(file, line, method) if object.respond_to?(:defined)
+    protected
+
+      def definitions
+        @definitions ||= []
       end
-    end
+
+      def definition?(event, method, klass, keyword_event, method_event)
+        event == keyword_event || (event == method_event && definition_method?(method, klass))
+      end
+
+      def definition_end?(event, method, klass)
+        !definitions.empty? && definition?(event, method, klass, 'end', 'c-return')
+      end
+
+      def definition_method?(method, klass)
+        (method == :initialize && klass.is_a?(Module)) || method.to_s =~ /^(class|instance|module)_(eval|exec)$/
+      end
+
+      def definition_start?(event, method, klass)
+        definition?(event, method, klass, 'class', 'c-call')
+      end
+
+      def trace_function(event, file, line, method, binding, klass)
+        if definition_start?(event, method, klass)
+          definitions << binding.eval('self')
+        elsif definition_end?(event, method, klass)
+          object = definitions.pop
+          method ||= object.class.name.downcase.to_sym
+          object.defined(file, line, method) if object.respond_to?(:defined)
+        end
+      end
   end
 end
 
--- test/defined_test.rb
+++ test/defined_test.rb
@@ -2,12 +2,10 @@ require 'test_helper'
 
 class DefinedTest < Test::Unit::TestCase
   module TestMethods
-    def called
-      @called
-    end
+    attr_reader :file, :line, :method, :called
 
     def defined(file, line, method)
-      @called = true
+      @file, @line, @method, @called = file, line, method, true
     end
   end
 
@@ -15,28 +13,59 @@ class DefinedTest < Test::Unit::TestCase
     extend TestMethods
   end
 
+  class Dummy
+    extend TestMethods
+  end
+
   def test_should_call_defined_when_module_is_defined
     assert Mock.called
+  end
+
+  def test_method_should_be_class_or_module_when_defined_with_keywords
+    assert_equal :module, Mock.method
+    assert_equal :class, Dummy.method
   end
 
   def test_should_call_defined_when_module_is_defined_with_new
     mod = Module.new { extend TestMethods }
     assert mod.called
+    assert_equal :initialize, mod.method
   end
 
   def test_should_call_defined_when_module_is_defined_with_class_eval
     Mock.class_eval { @called = false }
     assert Mock.called
+    assert_equal :class_eval, Mock.method
+  end
+
+  def test_should_call_defined_when_module_is_defined_with_class_exec
+    Mock.class_exec { @called = false }
+    assert Mock.called
+    assert_equal :class_exec, Mock.method
   end
 
   def test_should_call_defined_when_module_is_defined_with_instance_eval
     Mock.instance_eval { @called = false }
     assert Mock.called
+    assert_equal :instance_eval, Mock.method
+  end
+
+  def test_should_call_defined_when_module_is_defined_with_instance_exec
+    Mock.instance_exec { @called = false }
+    assert Mock.called
+    assert_equal :instance_exec, Mock.method
   end
 
   def test_should_call_defined_when_module_is_defined_with_module_eval
     Mock.module_eval { @called = false }
     assert Mock.called
+    assert_equal :module_eval, Mock.method
+  end
+
+  def test_should_call_defined_when_module_is_defined_with_module_exec
+    Mock.module_exec { @called = false }
+    assert Mock.called
+    assert_equal :module_exec, Mock.method
   end
 
   def test_should_match_definition_start?
@@ -58,8 +87,5 @@ class DefinedTest < Test::Unit::TestCase
   end
 
   def test_trace_function_should_remove_definition
-  end
-
-  def test_method_should_be_class_or_module_when_defined_with_keywords
   end
 end
