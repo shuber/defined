26d534f323183cbe2f7c4ae79cac7f5ba7d28652
Sean Huber <shuber@huberry.com>
2011-02-23 16:30:50 -0800

    reorder tests

--- test/defined_test.rb
+++ test/defined_test.rb
@@ -17,62 +17,6 @@ class DefinedTest < Test::Unit::TestCase
     extend TestMethods
   end
 
-  def test_should_not_call_defined_unless_implemented
-    mod = Module.new
-    assert !mod.respond_to?(:defined)
-  end
-
-  def test_should_call_defined_when_module_is_defined
-    assert Mock.called
-  end
-
-  def test_method_should_be_class_or_module_when_defined_with_keywords
-    assert_equal :module, Mock.method
-    assert_equal :class, Dummy.method
-  end
-
-  def test_should_call_defined_when_module_is_defined_with_new
-    mod = Module.new { extend TestMethods }
-    assert mod.called
-    assert_equal :initialize, mod.method
-  end
-
-  def test_should_call_defined_when_module_is_defined_with_class_eval
-    Mock.class_eval { @called = false }
-    assert Mock.called
-    assert_equal :class_eval, Mock.method
-  end
-
-  def test_should_call_defined_when_module_is_defined_with_class_exec
-    Mock.class_exec { @called = false }
-    assert Mock.called
-    assert_equal :class_exec, Mock.method
-  end
-
-  def test_should_call_defined_when_module_is_defined_with_instance_eval
-    Mock.instance_eval { @called = false }
-    assert Mock.called
-    assert_equal :instance_eval, Mock.method
-  end
-
-  def test_should_call_defined_when_module_is_defined_with_instance_exec
-    Mock.instance_exec { @called = false }
-    assert Mock.called
-    assert_equal :instance_exec, Mock.method
-  end
-
-  def test_should_call_defined_when_module_is_defined_with_module_eval
-    Mock.module_eval { @called = false }
-    assert Mock.called
-    assert_equal :module_eval, Mock.method
-  end
-
-  def test_should_call_defined_when_module_is_defined_with_module_exec
-    Mock.module_exec { @called = false }
-    assert Mock.called
-    assert_equal :module_exec, Mock.method
-  end
-
   def test_should_match_definition_start?
   end
 
@@ -88,12 +32,34 @@ class DefinedTest < Test::Unit::TestCase
   def test_should_not_match_end_without_start
   end
 
-  def test_trace_function_should_add_definition
+  def test_method_should_be_class_or_module_when_defined_with_keywords
+    assert_equal :module, Mock.method
+    assert_equal :class, Dummy.method
   end
 
-  def test_trace_function_should_remove_definition
+  def test_should_not_call_defined_unless_implemented
+    mod = Module.new
+    assert !mod.respond_to?(:defined)
   end
 
-  def test_trace_function_should_do_nothing
+  def test_should_call_defined_when_module_is_defined
+    assert Mock.called
+  end
+
+  def test_should_call_defined_when_module_is_defined_with_new
+    mod = Module.new { extend TestMethods }
+    assert mod.called
+    assert_equal :initialize, mod.method
+  end
+
+  %w(class instance module).each do |prefix|
+    %w(eval exec).each do |suffix|
+      method_name = [prefix, suffix].join('_').to_sym
+      define_method "test_should_call_defined_when_module_is_defined_with_#{method_name}" do
+        Mock.send(method_name) { @called = false }
+        assert Mock.called
+        assert_equal method_name, Mock.method
+      end
+    end
   end
 end
